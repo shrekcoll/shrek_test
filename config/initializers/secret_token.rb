d9cf74e413a976de8c5d74bb0815e5b288e231cb
root <root@rubyonrails.localdomain>
2012-10-11 10:22:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShrekDemo::Application.config.secret_token = '677abf644e7776659fca223b80ca90fafc7b871f7aa1c920a5f4aae68a113b90b045130ac205615366debb6cc038c098b550f4d414da1e4b2725111b5f8400b9'
